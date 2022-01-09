839241d91920fec12edf3931d6fb6eca79011740
kouci <mansourakociela@gmail.com>
2022-01-09 16:59:43 +0100

    'never'

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === 'production') {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     );
   }
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 app.listen(PORT,() => console.log(`Server startes on ${PORT}`))
 
 

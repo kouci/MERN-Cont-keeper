19e656f5d63239293846cffcc357e65dd928e86f
kouci <mansourakociela@gmail.com>
2022-01-09 17:16:49 +0100

    'nothing'

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const db = config.get('mongoURI')
 const connectDB = async () => {
     try{
     await mongoose
-    .connect(db, { useNewUrlParser: true })
+    .connect(process.env.MONGODB_URI || db, { useNewUrlParser: true })
     console.log("MongoDB connected ...")
     }catch(err) {
         console.log(err.message)

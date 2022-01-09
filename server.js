accf9cc44596dd190fb675ed5fac3046e5c6dfb8
kouci <mansourakociela@gmail.com>
2022-01-09 16:15:43 +0100

    updating code

--- server.js
+++ server.js
@@ -15,11 +15,15 @@ app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contacts', require('./routes/contacts'))
 
-if(process.env.NODE_ENV === 'production'){
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
     app.use(express.static('client/build'));
-    app.get("*", (req,res) => res.sendFile(path.resolve(__dirname,'client','build','index.html')));
-}
-
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
+  }
 const PORT = process.env.port || 5000
 app.listen(PORT,() => console.log(`Server startes on ${PORT}`))
 
